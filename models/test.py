fa1d7cbd2559d7933683eaa89188fb6d70a7c768
karthikbhamidipati <bssvkarthik@gmail.com>
2021-04-26 21:10:18 +0100

    Fixed issue with inference

--- models/test.py
+++ models/test.py
@@ -2,8 +2,11 @@ from _pickle import load
 from math import ceil
 from os.path import join
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from tqdm import tqdm
 
 from models import run_device
 from models.dataset import get_data_loaders
@@ -13,20 +16,33 @@ from models.train import compute_metrics
 from utils.config import FEATURES_PATH, MODELS, HYPER_PARAMETERS
 
 
-def _test(model, test_loader, criterion):
+def test(checkpoint_path, model_key, test_loader, criterion):
     test_loss, test_metrics = 0.0, np.zeros(2)
 
-    num_test_iters = ceil(len(test_loader.dataset) / test_loader.batch_size)
+    dataset_size = len(test_loader.dataset)
+    num_test_iters = ceil(dataset_size / test_loader.batch_size)
+    ground_truth_accent, predictions_accent = [], []
+    ground_truth_gender, predictions_gender = [], []
 
+    model = torch.load(join(checkpoint_path, model_key + ".pt"))
     model.eval()
-    for data, labels in test_loader:
+
+    for data, labels in tqdm(test_loader):
         # move data, labels to run_device
         data = data.to(run_device)
+
+        ground_truth_gender.extend(labels[0].tolist())
+        ground_truth_accent.extend(labels[1].tolist())
+
         labels = [label.to(run_device) for label in labels]
 
         # forward pass without grad to calculate the validation loss
         with torch.no_grad():
             outputs = model(data)
+
+            predictions_gender.extend(outputs[0].argmax(dim=1).cpu().tolist())
+            predictions_accent.extend(outputs[1].argmax(dim=1).cpu().tolist())
+
             loss = criterion(outputs, labels)
 
         # calculate validation loss
@@ -35,6 +51,8 @@ def _test(model, test_loader, criterion):
 
     print("Test Loss: {}, Test Metrics: {}"
           .format(test_loss / num_test_iters, test_metrics / num_test_iters))
+
+    return np.array(ground_truth_gender), np.array(predictions_gender), np.array(ground_truth_accent), np.array(predictions_accent)
 
 
 def test_model(root_dir, model_key, checkpoint_path):
@@ -45,6 +63,20 @@ def test_model(root_dir, model_key, checkpoint_path):
     data_loaders = get_data_loaders(features, config)
     loss_func = get_loss_function(config, features)
 
-    model = torch.load(join(checkpoint_path, model_key + ".pt"))
+    ground_truth_gender, predictions_gender, ground_truth_accent, predictions_accent = test(checkpoint_path, model_key,
+                                                                                            data_loaders['test'],
+                                                                                            loss_func)
+    plot_confusion_matrix(ground_truth_gender, predictions_gender,
+                          list(features['mappings']['gender']['idx2gender'].values()),
+                          "gender")
+    plot_confusion_matrix(ground_truth_accent, predictions_accent,
+                          list(features['mappings']['accent']['idx2accent'].values()),
+                          "accent")
 
-    _test(model, data_loaders['test'], loss_func)
+
+def plot_confusion_matrix(ground_truth, predictions, display_labels, fig_name):
+    cm = confusion_matrix(ground_truth, predictions)
+    display = ConfusionMatrixDisplay(confusion_matrix=cm,
+                                     display_labels=display_labels)
+    display.plot()
+    plt.savefig("figures/" + fig_name + ".png")
